fc1fcd0c6c187fe42aa0e9fcffaf29d1689e3442
bucanbenmutlu <bucanmutlubenmutlu@gmail.com>
2026-04-06 16:56:09 -0300

    Update models.py

--- Update backend/models.py
+++ Update backend/models.py
@@ -1,6 +1,1 @@
-def delete_event(event_id: int):
-    conn = get_connection()
-    cur = conn.cursor()
-    cur.execute("DELETE FROM events WHERE id = ?", (event_id,))
-    conn.commit()
-    conn.close()
+
