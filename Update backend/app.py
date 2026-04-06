0727016e340565b78046faf009f5c1c3f1c81a6f
bucanbenmutlu <bucanmutlubenmutlu@gmail.com>
2026-04-06 16:56:28 -0300

    Update app.py

--- Update backend/app.py
+++ Update backend/app.py
@@ -1,5 +1,1 @@
-from models import add_event, get_all_events, delete_event
-@app.route("/delete/<int:event_id>", methods=["POST"])
-def delete(event_id):
-    delete_event(event_id)
-    return redirect(url_for("index"))
+
